5914afae025df8a4cf36a49f7788dd5f13f485c4
hiroki-it <hasegawafeedshop@gmail.com>
2021-03-10 17:14:44 +0900

    update Slackにメッセージを送信できるようにした．

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,7 @@
 package main
 
-import(
-  "github.com/Hiroki-IT/notify_slack_of_amplify_events/cmd/amplify"
-  "github.com/Hiroki-IT/notify_slack_of_amplify_events/cmd/slack"
-)
+import "github.com/Hiroki-IT/notify_slack_of_amplify_events/cmd/slack"
 
 func main() {
-  slackMessage := slack_message.buildMessage()
-  slack_message.postMessage(slackMessage)
+    lambda.Start(handler)
 }
--- cmd/slack/slack.go
+++ cmd/slack/slack.go
@@ -6,52 +6,178 @@ import(
     "fmt"
     "log"
     "net/http"
+    "os"
 )
 
-// 構造体を定義し，JSONにマッピング
-type SlackMessage struct {
-    Token       string   `json:"token"`
-    Channel     string   `json:"channel"`
-    Text        string   `json:"text"`
-    Username    string   `json:"username"`
-//    Attachments []string `json:"attachments"`
-}
-
-func buildMessage() {
-    // URL
-    url := "https://xxxx.slack.com"
-
-    // ボディを定義する．
-    slackMessage := SlackMessage {
-        Token: "<トークン文字列>",
-        Channel: "<チャンネル名，もしくは@ユーザ名>",
-        Text: "<メッセージ>",
-        Username: "<as_userオプションがfalseの場合にBot名>",
-//         Attachments: [{
-//           // 任意のオプション     
-//           // 参考：
-//           // https://api.slack.com/messaging/composing/layouts#attachments
-//         }]
+/**
+ * EventBridgeから転送されたAmplifyイベントを構成します．
+ */
+type Request struct {
+    Records []struct { 
+        AmplifyEvent struct {
+            Event string `json:event`
+        }
     }
 }
 
-func postMessage(SlackMessage slackMessage) {
+/**
+ * Eventを構成します．
+ */
+type Event struct {
+    Version    string   `json:"version"`
+    Id         string   `json:"id"`
+    DetailType string   `json:"detail-type"`
+    Source     string   `json:"source"`
+    Account    string   `json:"account"`
+    Time       string   `json:"time"`
+    Region     string   `json:"region"`
+    Resources  []string `json:"resources"`
+    Detail     struct {
+        AppId      string `json:"appId"`
+        BranchName string `json:"branchName"`
+        JobId      string `json:"jobId"`
+        JobStatus  string `json:"jobStatus"`
+    } `json:"detail"`
+} `json:"event"`
+
+/**
+ * メッセージボディを構成します．
+ */
+type SlackMessage struct {
+    Channel string  `json:"channel"`
+    Text    string  `json:"text"`
+    Blocks  []Block `json:"blocks"`
+}
+
+/**
+ * Blockスライスの要素を構成します．
+ */
+type Block struct {
+    Type string `json:"type"`
+    Text struct {
+        Type string `json:"type"`
+        Text string `json:"text"`
+    } `json:"text,omitempty"`
+    Elements []Element `json:"elements,omitempty"`
+}
+
+/**
+ * Elementスライスの要素を構成します．
+ */
+type Element struct {
+    Type string `json:"type"`
+    Text string `json:"text"`
+}
+
+/**
+ * ハンドラー関数
+ */
+func handler(request Request) {
+
+    var event Event
+    
+    err := json.Unmarshal([]byte(request.Records[0].AmplifyEvent.Event), &event)
+    
+    if err != nil {
+        log.Printf("Failed: %#v\n", err)
+        return false
+    }
+
+    slackMessage := BuildMessage(event Event)
+  
+    return PostMessage(slackMessage)
+}
+
+/**
+ * Slackに送信するメッセージを構成します．
+ */
+func BuildMessage(event Event) SlackMessage {
+    return SlackMessage{
+        Channel: os.Getenv("SLACK_CHANNEL_ID"),
+        Text: "検証用dev環境",
+        Blocks: []Block{
+            Block{
+                Type: "section"
+                Element: {
+                    Type: "mrkdwn"
+                    Text: ":github: *検証用dev環境*" 
+                }
+            },
+            Block{
+                Type: "context"
+                Element: {
+                    Type: "mrkdwn"
+                    Text: Sprintf(
+                        "*結果*: %s",
+                        event.Detail.JobStatus
+                        )
+                }
+            },
+            Block{
+                Type: "context"
+                Element: {
+                    Type: "mrkdwn"
+                    Text: Sprintf(
+                        "*ブランチ名*: %s",
+                        event.Detail.BranchName
+                        )
+                }
+            },            
+            Block{
+                Type: "context"
+                Element: {
+                    Type: "mrkdwn"
+                    Text: Sprintf(
+                        "*検証URL*: https://%s.%s.amplifyapp.com", 
+                        event.Detail.BranchName,
+                        event.Detail.AppId
+                        )
+                }            
+            },
+            Block{
+                Type: "context"
+                Element: {
+                    Type: "mrkdwn"
+                    Text: Sprintf(
+                        ":amplify: <https://%s.console.aws.amazon.com/amplify/home?region=%s#/%s/%s/%s|*Amplifyコンソール画面はこちら*>",
+                        event.Region,
+                        event.Region,
+                        event.Detail.AppId,
+                        event.Detail.BranchName,
+                        event.Detail.JobId,
+                        )
+                }            
+            },
+            Block{
+                Type: "divider"
+            }
+        }
+    }
+}
+
+/**
+ * Slackにメッセージを送信します．
+ */
+func PostMessage(slackMessage SlackMessage) bool {
+
     // マッピングを元に，構造体をJSONに変換する．
     json, err := json.Marshal(slackMessage)
 
     if err != nil {
-        log.Fatalf("ERROR: %#v\n", err)
+        log.Printf("Failed: %#v\n", err)
+        return false
     }
 
     // リクエストメッセージを定義する．
     request, err := http.NewRequest(
         "POST",
-        url,
+        "https://xxxx.slack.com",
         bytes.NewBuffer(json),
     )
 
     if err != nil {
-        log.Fatalf("ERROR: %#v\n", err)
+        log.Printf("Failed: %#v\n", err)
+        return false
     }
 
     // ヘッダーを定義する．
@@ -66,12 +192,16 @@ func postMessage(SlackMessage slackMessage) {
     defer response.Body.Close()
     
     if err != nil {
-        log.Fatalf("ERROR: %#v\n", err)
+        log.Printf("Failed: %#v\n", err)
+        return false
     }
     
     if response.StatusCode != 200 {
-        log.Fatalf("ERROR: %#v\n", response)
+        log.Printf("Failed: %#v\n", response)
+        return false
     }
     
-    fmt.Printf("INFO: %#v\n", response)
+    fmt.Printf("Success %#v\n", response)
+    
+    return true
 }
