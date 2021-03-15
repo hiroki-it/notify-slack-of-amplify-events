abfe7af12a01119c5786f1d01bf3a1ad3f1fb62f
hiroki-it <hasegawafeedshop@gmail.com>
2021-03-15 13:21:07 +0900

    update ライブラリをimportできるようにした．

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/Hiroki-IT/notify_slack_of_amplify_events/cmd/slack"
+import (
+    "github.com/aws/aws-lambda-go/lambda"
+    "github.com/Hiroki-IT/notify_slack_of_amplify_events/cmd/slack"
+    )
 
 func main() {
     lambda.Start(handler)
