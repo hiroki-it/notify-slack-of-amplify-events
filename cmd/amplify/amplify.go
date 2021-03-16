8c0c05f82109bd6bb9d13f1ef64ded0ae858877a
hiroki-it <hasegawafeedshop@gmail.com>
2021-03-16 21:39:35 +0900

    update Slackにメッセージを送信できるようにした．

--- cmd/amplify/amplify.go
+++ cmd/amplify/amplify.go
@@ -4,3 +4,29 @@ import(
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/amplify"
 )
+
+/**
+ * AppOption構造体
+ */
+type AppOption struct {
+    AppId string `json:"appId"`
+}
+
+func GetAmplifyApp() amplifyApp {
+
+    session, err := session.Must(
+        session.NewSession(
+            aws.NewConfig().WithRegion("ap-northeast-1")
+        )
+    )
+    
+    if err != nil {
+        log.Fatalf("Failed: %#v\n", err)
+    }
+    
+    amplify := amplify.New(session)
+    
+    appOption
+    
+    response = amplify.GetApp()
+}
