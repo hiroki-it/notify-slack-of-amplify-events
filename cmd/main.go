4f6f0e7ae7544d9db94abaf486ba808de720b0af
hiroki-it <hasegawafeedshop@gmail.com>
2021-04-06 19:35:07 +0900

    update envファイルを読み込めるようにした．

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/hiroki-it/notify_slack_of_amplify_events/cmd/slack"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	godotenv.Load()
+
 	lambda.Start(slack.LambdaHandler)
 }
