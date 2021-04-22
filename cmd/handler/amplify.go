eca8cf46d414e14e69d5bdef460bee8abe4f1363
hiroki-it <hasegawafeedshop@gmail.com>
2021-04-22 23:01:17 +0900

    update 実装の場所を変更した．

--- cmd/handler/amplify.go
+++ cmd/handler/amplify.go
@@ -8,6 +8,27 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/amplify"
 )
 
+type Event struct {
+	Version    string   `json:"version"`
+	Id         string   `json:"id"`
+	DetailType string   `json:"detail-type"`
+	Source     string   `json:"source"`
+	Account    string   `json:"account"`
+	Time       string   `json:"time"`
+	Region     string   `json:"region"`
+	Resources  []string `json:"resources"`
+	Detail     struct {
+		AppId      string `json:"appId"`
+		BranchName string `json:"branchName"`
+		JobId      string `json:"jobId"`
+		JobStatus  string `json:"jobStatus"`
+	} `json:"detail"`
+}
+
+type AmplifyBranch struct {
+	DisplayName string
+}
+
 type AmplifyClientInterface interface {
 	getBranchFromAmplify(event Event) (*amplify.GetBranchOutput, error)
 }
--- cmd/handler/handler.go
+++ cmd/handler/handler.go
@@ -16,27 +16,6 @@ type Request struct {
 	}
 }
 
-type Event struct {
-	Version    string   `json:"version"`
-	Id         string   `json:"id"`
-	DetailType string   `json:"detail-type"`
-	Source     string   `json:"source"`
-	Account    string   `json:"account"`
-	Time       string   `json:"time"`
-	Region     string   `json:"region"`
-	Resources  []string `json:"resources"`
-	Detail     struct {
-		AppId      string `json:"appId"`
-		BranchName string `json:"branchName"`
-		JobId      string `json:"jobId"`
-		JobStatus  string `json:"jobStatus"`
-	} `json:"detail"`
-}
-
-type AmplifyBranch struct {
-	DisplayName string
-}
-
 /**
  * Lambdaハンドラー関数
  */
