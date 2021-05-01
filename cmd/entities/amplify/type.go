bbaf3ee191c1777b43f411da3f6a92d8684a99ac
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-01 18:15:55 +0900

    update AmplifyClientのインターフェースを実装した．

--- cmd/entities/amplify/type.go
+++ cmd/entities/amplify/type.go
@@ -1,12 +1,19 @@
 package amplify
 
 import (
+	aws_amplify "github.com/aws/aws-sdk-go/service/amplify"
 	"github.com/aws/aws-sdk-go/service/amplify/amplifyiface"
+	"github.com/hiroki-it/notify-slack-of-amplify-events/cmd/entities/eventbridge"
 )
+
+type AmplifyClientInterface interface {
+	GetBranchFromAmplify(eventDetail *eventbridge.EventDetail) (*aws_amplify.GetBranchOutput, error)
+}
 
 /**
  * Amplifyのクライアントを構成します．
  */
 type AmplifyClient struct {
+	AmplifyClientInterface
 	Api amplifyiface.AmplifyAPI
 }
