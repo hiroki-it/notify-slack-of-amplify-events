d95a41cc209fba38cbc0955b4a498c2525ba9763
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-05 21:10:20 +0900

    update エラーをロギングできるようにした．

--- cmd/usecases/logger/logger.go
+++ cmd/usecases/logger/logger.go
@@ -2,11 +2,17 @@ package logger
 
 import (
 	"log"
+
+	"github.com/hiroki-it/notify-slack-of-amplify-events/cmd/usecases/exception"
 )
 
 /**
  * エラーをロギングします．
  */
-func ErrorLog(err error) {
-	log.Printf("ERROR: %+v\n", err)
+func ErrorLog(exception *exception.Exception) {
+	log.Printf(
+		"ERROR: %s %+v\n",
+		exception.GetMessage(),
+		exception.GetStackTrace(),
+	)
 }
