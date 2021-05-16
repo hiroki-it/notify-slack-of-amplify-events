363508313889d529e60fd413e0df0f7d0d342c3a
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-16 22:42:07 +0900

    update アプリケーション環境に応じて，クレデンシャル情報を切り替えるようにした．

---
+++ ops/export_aws_credentials.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+case $APP_ENV in
+    "stg")
+        AWS_ACCOUNT_ID="$STG_AWS_ACCOUNT_ID"
+        AWS_ACCESS_KEY_ID="$STG_AWS_ACCESS_KEY_ID"
+        AWS_SECRET_ACCESS_KEY="$STG_AWS_SECRET_ACCESS_KEY"
+    ;;
+    "prd")
+        AWS_ACCOUNT_ID="$PRD_AWS_ACCOUNT_ID"
+        AWS_ACCESS_KEY_ID="$PRD_AWS_ACCESS_KEY_ID"
+        AWS_SECRET_ACCESS_KEY="$PRD_AWS_SECRET_ACCESS_KEY"
+    ;;
+    *)
+        echo "The parameter ${ENV} is invalid."
+        exit 1
+    ;;
+esac
+
+cat << EOT > "aws_envs.sh"
+export AWS_ACCOUNT_ID="$AWS_ACCOUNT_ID"
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+EOT
