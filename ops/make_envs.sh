a8b0300fa7343da87c05b8a38695b5885380910f
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-19 03:01:52 +0900

    update クレデンシャルファイルを作成するようにした．

--- ops/make_envs.sh
+++ ops/make_envs.sh
@@ -28,8 +28,5 @@ EOF
 
 cat << EOT > "envs.sh"
 export APP_ENV="$APP_ENV"
-export AWS_ACCOUNT_ID
-export AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY
 export AWS_ECR_ACCOUNT_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 EOT
