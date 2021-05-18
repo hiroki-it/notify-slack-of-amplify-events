fdbda0874a9bbc42ee811d7f1b58ce47314c3790
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-18 22:37:44 +0900

    fix 環境変数を出力できるようにした．

--- ops/make_aws_envs.sh
+++ ops/make_aws_envs.sh
@@ -24,6 +24,4 @@ export APP_ENV="$APP_ENV"
 export AWS_ACCOUNT_ID
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
-export AWS_ECR_ACCOUNT_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
-export AWS_ECR_REPOSITORY="${APP_ENV}-notify-slack-of-amplify-events-repository"
 EOT
