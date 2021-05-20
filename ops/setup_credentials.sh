2d7a500870dc22dee6f4f1bfdf02247d07557dbb
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-21 02:16:54 +0900

    update credentialsの作成にorbsを使用しないようにした．

---
+++ ops/setup_credentials.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+aws configure --profile ${APP_ENV} << EOF
+$(echo "$AWS_ACCESS_KEY_ID")
+$(echo "$AWS_SECRET_ACCESS_KEY")
+$(echo "$AWS_DEFAULT_REGION")
+json
+EOF
