d818183931e9b736ae1bf3dadfaaf3474535ee76
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-21 02:36:10 +0900

    update 鉤括弧を削除した．

--- ops/setup_credentials.sh
+++ ops/setup_credentials.sh
@@ -2,7 +2,7 @@
 
 set -xeuo pipefail
 
-aws configure --profile ${APP_ENV} << EOF
+aws configure --profile $APP_ENV << EOF
 $(echo "$AWS_ACCESS_KEY_ID")
 $(echo "$AWS_SECRET_ACCESS_KEY")
 $(echo "$AWS_DEFAULT_REGION")
