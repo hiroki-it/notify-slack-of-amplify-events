70a583b294593bdc21f67b5d69bfb8f7ce6a3ac5
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-22 18:10:34 +0900

    fix 環境変数を出力できるようにした．

--- ops/setup_envs.sh
+++ ops/setup_envs.sh
@@ -18,12 +18,3 @@ case "$APP_ENV" in
         exit 1
     ;;
 esac
-
-# CircleCIの環境変数として，値を出力する．
-echo 'export AWS_ACCOUNT_ID' >> $BASH_ENV
-echo 'export AWS_ACCESS_KEY_ID' >> $BASH_ENV
-echo 'export AWS_SECRET_ACCESS_KEY' >> $BASH_ENV
-echo 'export AWS_ECR_ACCOUNT_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"' >> $BASH_ENV
-
-# 値が正しく出力されたかを確認する．
-printenv | sort -f
