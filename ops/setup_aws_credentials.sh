f4d7523cfca0670d520cec94166882e84ea2cdf9
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-22 17:16:54 +0900

    fix 環境変数を出力できるようにした．

--- ops/setup_aws_credentials.sh
+++ ops/setup_aws_credentials.sh
@@ -4,9 +4,9 @@ set -xeuo pipefail
 
 # defaultプロファイルにクレデンシャル情報を設定する．
 aws configure << EOF
-$(echo "$AWS_ACCESS_KEY_ID")
-$(echo "$AWS_SECRET_ACCESS_KEY")
-$(echo "$AWS_DEFAULT_REGION")
+$(echo $AWS_ACCESS_KEY_ID)
+$(echo $AWS_SECRET_ACCESS_KEY)
+$(echo $AWS_DEFAULT_REGION)
 json
 EOF
 
--- ops/setup_envs.sh
+++ ops/setup_envs.sh
@@ -4,14 +4,14 @@ set -xeuo pipefail
 
 case $APP_ENV in
     "stg")
-        AWS_ACCOUNT_ID="$STG_AWS_ACCOUNT_ID"
-        AWS_ACCESS_KEY_ID="$STG_AWS_ACCESS_KEY_ID"
-        AWS_SECRET_ACCESS_KEY="$STG_AWS_SECRET_ACCESS_KEY"
+        AWS_ACCOUNT_ID=$STG_AWS_ACCOUNT_ID
+        AWS_ACCESS_KEY_ID=$STG_AWS_ACCESS_KEY_ID
+        AWS_SECRET_ACCESS_KEY=$STG_AWS_SECRET_ACCESS_KEY
     ;;
     "prd")
-        AWS_ACCOUNT_ID="$PRD_AWS_ACCOUNT_ID"
-        AWS_ACCESS_KEY_ID="$PRD_AWS_ACCESS_KEY_ID"
-        AWS_SECRET_ACCESS_KEY="$PRD_AWS_SECRET_ACCESS_KEY"
+        AWS_ACCOUNT_ID=$PRD_AWS_ACCOUNT_ID
+        AWS_ACCESS_KEY_ID=$PRD_AWS_ACCESS_KEY_ID
+        AWS_SECRET_ACCESS_KEY=$PRD_AWS_SECRET_ACCESS_KEY
     ;;
     *)
         echo "The parameter ${ENV} is invalid."
@@ -20,11 +20,11 @@ case $APP_ENV in
 esac
 
 # CircleCIの環境変数として，値を出力する．
-echo 'export APP_ENV="$APP_ENV"' >> $BASH_ENV
-echo 'export AWS_ACCOUNT_ID' >> $BASH_ENV
-echo 'export AWS_ACCESS_KEY_ID' >> $BASH_ENV
-echo 'export AWS_SECRET_ACCESS_KEY' >> $BASH_ENV
-echo 'export AWS_ECR_ACCOUNT_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"' >> $BASH_ENV
+echo "export APP_ENV=$APP_ENV" >> $BASH_ENV
+echo "export AWS_ACCOUNT_ID" >> $BASH_ENV
+echo "export AWS_ACCESS_KEY_ID" >> $BASH_ENV
+echo "export AWS_SECRET_ACCESS_KEY" >> $BASH_ENV
+echo "export AWS_ECR_ACCOUNT_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com" >> $BASH_ENV
 
 # 値が正しく出力されたかを確認する．
 printenv | sort -f
