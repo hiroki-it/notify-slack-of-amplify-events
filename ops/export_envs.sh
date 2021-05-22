62371546d5771fdf7902564762a2d0322b6b96d2
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-22 20:06:59 +0900

    update BASH_ENVを読み込んで環境変数を出力する処理をファイルに切り分けた．

---
+++ ops/export_envs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+echo 'export AWS_ECR_ACCOUNT_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"' >> $BASH_ENV
+
+# 環境変数を出力します．
+source $BASH_ENV
