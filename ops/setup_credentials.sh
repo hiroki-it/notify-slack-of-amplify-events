e3996cf5718bfd8eb164f6964bb197bc4989c926
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-22 16:32:55 +0900

    update defaultプロファイルに値を設定するようにした．

--- ops/setup_credentials.sh
+++ ops/setup_credentials.sh
@@ -2,9 +2,13 @@
 
 set -xeuo pipefail
 
-aws configure --profile $APP_ENV << EOF
+# defaultプロファイルにクレデンシャル情報を設定する．
+aws configure << EOF
 $(echo "$AWS_ACCESS_KEY_ID")
 $(echo "$AWS_SECRET_ACCESS_KEY")
 $(echo "$AWS_DEFAULT_REGION")
 json
 EOF
+
+# 正しく設定されたかを確認する．
+aws configure list
