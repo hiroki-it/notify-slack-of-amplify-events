2fe81621261d26d3d0c9f00eb3ce5dfcb5c06a85
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-12 18:46:03 +0900

    fix 不要な実装を削除した．

--- cmd/entities/eventbridge/types.go
+++ cmd/entities/eventbridge/types.go
@@ -1,16 +1,9 @@
 package eventbridge
 
 /**
- * EventDetailインターフェースを構成します．
- */
-type EventDetailInterface interface {
-}
-
-/**
  * EventDetailインターフェースの実装を構成します．
  */
 type EventDetail struct {
-	EventDetailInterface
 	AppId      string `json:"appId"`
 	BranchName string `json:"branchName"`
 	JobId      string `json:"jobId"`
