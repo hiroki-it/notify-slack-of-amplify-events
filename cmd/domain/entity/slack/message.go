5c12f9fff8814d0a4564c8493fe48937239444ea
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-18 10:30:12 +0900

    update 実装するファイルを変更した．

--- cmd/domain/entity/slack/message.go
+++ cmd/domain/entity/slack/message.go
@@ -11,6 +11,40 @@ import (
 )
 
 /**
+ * Slackメッセージを構成します．
+ */
+type SlackMessage struct {
+	Channel     string       `json:"channel"`
+	Text        string       `json:"text"`
+	Attachments []Attachment `json:"attachments"`
+}
+
+/**/
+type Attachment struct {
+	Color  string  `json:"color"`
+	Blocks []Block `json:"blocks"`
+}
+
+/**/
+type Block struct {
+	Type     string    `json:"type"`
+	Text     *Text     `json:"text,omitempty"`
+	Elements []Element `json:"elements,omitempty"`
+}
+
+/**/
+type Text struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+/**/
+type Element struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+/**
  * コンストラクタ
  * Messageを作成します．
  */
