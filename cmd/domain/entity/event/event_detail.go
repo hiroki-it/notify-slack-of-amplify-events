78faac333c509127f21900a63cbd35458ed5ef0d
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-19 14:44:01 +0900

    update コンストラクタのないオブジェクトとして，EventDetailを実装した／

---
+++ cmd/domain/entity/event/event_detail.go
@@ -0,0 +1,30 @@
+package event
+
+/**
+ * EventDetailを構成します．
+ */
+type EventDetail struct {
+	AppId      string `json:"appId"`
+	BranchName string `json:"branchName"`
+	JobId      string `json:"jobId"`
+	JobStatus  string `json:"jobStatus"`
+}
+
+/**
+ * JobStatusを返却します．
+ */
+func (eventDetail *EventDetail) GetJobStatus() string {
+	return eventDetail.JobStatus
+}
+
+/**
+ * ジョブステータスが成功かどうかを判定します．
+ */
+func (eventDetail *EventDetail) IsSucceed() bool {
+
+	if eventDetail.JobStatus == "SUCCEED" {
+		return true
+	}
+
+	return false
+}
