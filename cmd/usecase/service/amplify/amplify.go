a6f6fece890124c9cf3dd11567855a518aab7ff2
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-21 23:22:18 +0900

    update 値オブジェクトから属性を取得する時に，ひとまずValueメソッドを使用しないようにした．

--- cmd/usecase/service/amplify/amplify.go
+++ cmd/usecase/service/amplify/amplify.go
@@ -41,8 +41,8 @@ func NewAmplifyClient(amplifyApi amplifyiface.AmplifyAPI) *AmplifyClient {
 func (cl *AmplifyClient) GetBranchFromAmplify(detail *detail.Detail) (*aws_amplify.GetBranchOutput, error) {
 
 	getBranchInput := &aws_amplify.GetBranchInput{
-		AppId:      aws.String(detail.AppId.Value()),
-		BranchName: aws.String(detail.BranchName.Value()),
+		AppId:      aws.String(detail.AppId),
+		BranchName: aws.String(detail.BranchName),
 	}
 
 	// ブランチ情報を構造体として取得します．
