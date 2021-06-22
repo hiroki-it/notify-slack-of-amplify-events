0f58779549e8c858c85a2374180c373dadd63178
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-23 02:44:51 +0900

    add AppIdを値オブジェクトとして新しく実装した．

---
+++ cmd/domain/entity/detail/app_id.go
@@ -0,0 +1,34 @@
+package detail
+
+import (
+	"encoding/json"
+
+	"github.com/hiroki-it/notify-slack-of-amplify-events/cmd/domain/core"
+)
+
+type AppId struct {
+	core.ValueObject
+	value string
+}
+
+func NewAppId(value string) *AppId {
+
+	return &AppId{
+		value: value,
+	}
+}
+
+// Equals 等価性を検証します．
+func (ai AppId) Equals(target AppId) bool {
+	return ai.value == target.value
+}
+
+// Value 属性を返却します．
+func (ai AppId) Value() string {
+	return ai.value
+}
+
+// MarshalJSON 構造体をJSONに変換します．
+func (ai AppId) MarshalJSON() ([]byte, error) {
+	return json.Marshal(ai.value)
+}
