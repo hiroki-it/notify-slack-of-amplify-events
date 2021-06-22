ff46f44ff2350fb4d398b0176d8e161b3d91738d
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-23 02:45:00 +0900

    add BranchNameを値オブジェクトとして新しく実装した．

---
+++ cmd/domain/entity/detail/branch_name.go
@@ -0,0 +1,35 @@
+package detail
+
+import (
+	"encoding/json"
+
+	"github.com/hiroki-it/notify-slack-of-amplify-events/cmd/domain/core"
+)
+
+type BranchName struct {
+	core.ValueObject
+	value string
+}
+
+// NewBranchName コンストラクタ
+func NewBranchName(value string) *BranchName {
+
+	return &BranchName{
+		value: value,
+	}
+}
+
+// Equals 等価性を検証します．
+func (bn *BranchName) Equals(target BranchName) bool {
+	return bn.value == target.Value()
+}
+
+// Value 属性を返却します．
+func (bn *BranchName) Value() string {
+	return bn.value
+}
+
+// MarshalJSON 構造体をJSONに変換します．
+func (bn *BranchName) MarshalJSON() ([]byte, error) {
+	return json.Marshal(bn.Value())
+}
