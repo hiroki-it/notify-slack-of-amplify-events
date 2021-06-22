ed4b3a0ebf665c8372ddd4aa4342eaf21a32126b
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-23 02:45:17 +0900

    add JobStatusを値オブジェクトとして新しく実装した．

---
+++ cmd/domain/entity/detail/job_status.go
@@ -0,0 +1,34 @@
+package detail
+
+import (
+	"encoding/json"
+
+	"github.com/hiroki-it/notify-slack-of-amplify-events/cmd/domain/core"
+)
+
+type JobStatus struct {
+	core.ValueObject
+	value string
+}
+
+func NewJobStatus(value string) *JobStatus {
+
+	return &JobStatus{
+		value: value,
+	}
+}
+
+// Equals 等価性を検証します．
+func (js *JobStatus) Equals(target JobStatus) bool {
+	return js.value == target.Value()
+}
+
+// Value 属性を返却します．
+func (js *JobStatus) Value() string {
+	return js.value
+}
+
+// MarshalJSON 構造体をJSONに変換します．
+func (js JobStatus) MarshalJSON() ([]byte, error) {
+	return json.Marshal(js.Value())
+}
