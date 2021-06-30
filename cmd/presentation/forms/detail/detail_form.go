32422a0ba0b11641274c27d72db5fb40328fccfe
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-01 08:58:58 +0900

    add バリデーションフォームを新しく実装した．

---
+++ cmd/presentation/forms/detail/detail_form.go
@@ -0,0 +1,24 @@
+package detail
+
+import (
+	"github.com/go-playground/validator"
+)
+
+type DetailForm struct {
+	AppId      string `json:"appId" validate:"required"`
+	BranchName string `json:"branchName" validate:"required"`
+	JobId      string `json:"jobId" validate:"required"`
+	JobStatus  string `json:"jobStatus" validate:"required"`
+}
+
+// NewDetailForm コンストラクタ
+func NewDetailForm() *DetailForm {
+
+	return &DetailForm{}
+}
+
+// Validate バリデーションを実行します．
+func (f *DetailForm) Validate() error {
+	v := validator.New()
+	return v.Struct(f)
+}
