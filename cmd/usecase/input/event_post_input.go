5414b41d3f9069518990113d2b2f63bdeff59122
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-01 08:58:37 +0900

    add インプットを新しく実装した．

---
+++ cmd/usecase/input/event_post_input.go
@@ -0,0 +1,18 @@
+package input
+
+type EventPostInput struct {
+	AppId      string
+	BranchName string
+	JobId      string
+	JobStatus  string
+}
+
+func NewEventPostInput(appId string, branchName string, jobId string, JobStatus string) *EventPostInput {
+
+	return &EventPostInput{
+		AppId:      appId,
+		BranchName: branchName,
+		JobId:      jobId,
+		JobStatus:  JobStatus,
+	}
+}
