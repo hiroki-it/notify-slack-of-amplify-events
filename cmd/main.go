a600e3a2b11981e74e6b89d320b7e3d2dd1287e3
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-01 08:59:55 +0900

    update コントローラに依存対象のユースケースを注入した．

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/hiroki-it/notify-slack-of-amplify-events/cmd/presentation/controllers"
+	"github.com/hiroki-it/notify-slack-of-amplify-events/cmd/usecase/usecases"
 )
 
 func main() {
-	lambda.Start(handler.HandleRequest)
+	uc := usecases.NewEventPostUseCase()
+	c := controllers.NewLambdaController(uc)
+	lambda.Start(c.PostEvent)
 }
