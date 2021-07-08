5b57485bbdc9299cc880db11f891b15ed912508c
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-09 01:11:38 +0900

    add パス値オブジェクトを新しく実装した．

---
+++ cmd/domain/file/path.go
@@ -0,0 +1,27 @@
+package file
+
+import (
+	"github.com/hiroki-it/notify-slack-of-amplify-events/cmd/domain/core"
+)
+
+type Path struct {
+	path string
+}
+
+// NewPath コンストラクタ
+func NewPath(path string) *Path {
+
+	return &Path{
+		path: path,
+	}
+}
+
+// Path 属性を返却します．
+func (p *Path) Path() string {
+	return p.path
+}
+
+// Equals 等価性を検証します．
+func (p *Path) Equals(target core.ValueObject) bool {
+	return p.path == target.(*Path).Path()
+}
