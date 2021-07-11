bcf98da20969fdada8e8eb932006b7588c84d0fe
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-11 22:43:11 +0900

    update アップキャストできるように，構造体を委譲するのではなく，インターフェースを委譲するようにした．

--- cmd/domain/detail/app_id.go
+++ cmd/domain/detail/app_id.go
@@ -5,20 +5,25 @@ import (
 )
 
 type AppId struct {
-	*core.ID
 	id string
 }
+
+var _ core.ID = &AppId{}
 
 // NewAppId コンストラクタ
 func NewAppId(id string) *AppId {
 
 	return &AppId{
-		ID: &core.ID{},
 		id: id,
 	}
 }
 
+// Id 属性を返却します．
+func (ai *AppId) Id() string {
+	return ai.id
+}
+
 // Equals 等価性を検証します．
-func (ai *AppId) Equals(target *AppId) bool {
-	return ai.id == target.Id()
+func (ai *AppId) Equals(target core.ID) bool {
+	return ai.id == target.(*AppId).Id()
 }
--- cmd/domain/detail/job_id.go
+++ cmd/domain/detail/job_id.go
@@ -5,20 +5,25 @@ import (
 )
 
 type JobId struct {
-	*core.ID
 	id string
 }
+
+var _ core.ID = &JobId{}
 
 // NewJobId コンストラクタ
 func NewJobId(id string) *JobId {
 
 	return &JobId{
-		ID: &core.ID{},
 		id: id,
 	}
 }
 
+// Id 属性を返却します．
+func (ji *JobId) Id() string {
+	return ji.id
+}
+
 // Equals 等価性を検証します．
-func (ji *JobId) Equals(target *JobId) bool {
-	return ji.id == target.Id()
+func (ji *JobId) Equals(target core.ID) bool {
+	return ji.id == target.(*JobId).Id()
 }
